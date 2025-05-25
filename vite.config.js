715a005ac55a459745610a4a39afea037b0d25f2
15lu.akari <92751432+mikaeloganesian@users.noreply.github.com>
2025-05-25 23:07:42 +0300

    first push

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ← ВАЖНО! Должен быть именно "/"
+  base: '/hsehelperweb/', // ← ВАЖНО! Должен быть именно "/"
 })
